3b2229e53075eb5d8ae62461db13840d116694e2
Venture <wcdeng0331@gmail.com>
2024-03-03 22:19:55 +0800

    增加路由规则

--- cmd/rps_service/main.go
+++ cmd/rps_service/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"RPS_SERVICE/pkg/router"
 	"RPS_SERVICE/pkg/websocket"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// 设置WebSocket路由
-	http.HandleFunc("/ws", websocket.HandleConnections)
+	// 设置WebSocket和其他路由
+	router.SetupRoutes()
 
 	// 启动新的协程，用于监听并处理WebSocket消息
 	go websocket.HandleMessages()
-
+	fmt.Println("WS_HTTP server started on :8080")
 	// 启动服务器
 	log.Println("HTTP server started on :8080")
 	err := http.ListenAndServe(":8080", nil)
